8ebd4e2761c215b9b8ba1ea9badedd1555ea06fd
Elrisas <s.valencia29@ciaf.edu.co>
2024-05-02 19:31:39 -0500

    10

--- p10/r10.js
+++ p10/r10.js
@@ -1,16 +1,1 @@
-const numeroSecreto = Math.floor(Math.random() * 100) + 1;
-let intentos = 0;
-
-while (true) {
-    const numeroUsuario = parseInt(prompt('Adivina el número secreto (entre 1 y 100):'));
-    intentos++;
-
-    if (numeroUsuario === numeroSecreto) {
-        console.log(`¡Correcto! Adivinaste en ${intentos} intentos.`);
-        break;
-    } else if (numeroUsuario < numeroSecreto) {
-        console.log('El número secreto es mayor.');
-    } else {
-        console.log('El número secreto es menor.');
-    }
-}
+//Profe es la misma pregunat del numero cinco 
